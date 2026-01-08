98c92cc68e4b561f4d9a897a1682cf4bed5b870f
knw <sky_9233@naver.com>
2026-01-08 10:18:57 +0900

    Phase 1 complete: tuple, get, tuple_size
    - tuple class with recursive inheritance
    - get<I> with if constexpr
    - tuple_size trait with sizeof...
    - All tests passing

--- include/mini-tuple/tuple.hpp
+++ include/mini-tuple/tuple.hpp
@@ -1,10 +1,55 @@
-#ifndef MINI_TUPLE_TUPLE_HPP
-#define MINI_TUPLE_TUPLE_HPP
+#pragma once
 
 namespace mini {
 
-// TODO: tuple 구현 예정
+// 기본 템플릿 (variadic)
+template<typename... Ts>
+class tuple;
+
+// get 함수 전방 선언
+template<size_t I, typename Head, typename... Tail>
+auto& get(tuple<Head, Tail...>& t);
+
+// 재귀 케이스: Head + Tail...
+template<typename Head, typename... Tail>
+class tuple<Head, Tail...> : public tuple<Tail...> {
+private:
+    Head value_;
+
+public:
+    tuple(Head h, Tail... t) 
+        : tuple<Tail...>(t...), value_(h)
+    {}
+    
+    // friend 선언 (다른 이름 사용)
+    template<size_t I, typename H, typename... T>
+    friend auto& get(tuple<H, T...>& t);
+};
+
+// 종료 조건: 빈 tuple
+template<>
+class tuple<> {};
+
+// get 구현
+template<size_t I, typename Head, typename... Tail>
+auto& get(tuple<Head, Tail...>& t) {
+    if constexpr (I == 0) {
+        return t.value_;
+    } else {
+        return get<I-1>(static_cast<tuple<Tail...>&>(t));
+    }
+}
+
+// tuple_size
+template<typename T>
+struct tuple_size;
+
+template<typename... Ts>
+struct tuple_size<tuple<Ts...>> {
+    static constexpr size_t value = sizeof...(Ts);
+};
+
+template<typename T>
+inline constexpr size_t tuple_size_v = tuple_size<T>::value;
 
 }  // namespace mini
-
-#endif  // MINI_TUPLE_TUPLE_HPP
--- test_main.cpp
+++ test_main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <string>
 
-// TODO: #include "mini-tuple/tuple.hpp"
+#include "mini-tuple/tuple.hpp"
 
 int main() {
     std::cout << "=== Mini Tuple Test ===\n\n";
 
     // Phase 1: 기본 tuple 생성
-    // mini::tuple<int, double, std::string> t{42, 3.14, "hello"};
-    // std::cout << "Created tuple<int, double, string>\n";
+    mini::tuple<int, double, std::string> t{42, 3.14, "hello"};
+    std::cout << "Created tuple<int, double, string>\n";
 
-    // Phase 1: get<I> 테스트
-    // auto num = mini::get<0>(t);
-    // auto pi = mini::get<1>(t);
-    // auto str = mini::get<2>(t);
-    // std::cout << "get<0>: " << num << "\n";
-    // std::cout << "get<1>: " << pi << "\n";
-    // std::cout << "get<2>: " << str << "\n";
+    // get 테스트
+    std::cout << "get<0>: " << mini::get<0>(t) << "\n";
+    std::cout << "get<1>: " << mini::get<1>(t) << "\n";
+    std::cout << "get<2>: " << mini::get<2>(t) << "\n";
 
-    // Phase 1: tuple_size 테스트
-    // constexpr auto size = mini::tuple_size_v<decltype(t)>;
-    // std::cout << "tuple_size: " << size << "\n";
+    // 수정 테스트
+    mini::get<0>(t) = 100;
+    std::cout << "After modification, get<0>: " << mini::get<0>(t) << "\n";
+
+    // tuple_size 테스트
+    constexpr auto size = mini::tuple_size_v<decltype(t)>;
+    std::cout << "tuple_size: " << size << "\n";
 
     std::cout << "\n=== All tests passed! ===\n";
     return 0;
